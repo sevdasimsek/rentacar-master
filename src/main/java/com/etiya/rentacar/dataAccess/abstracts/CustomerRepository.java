49c6e37c7f84111a85606433227dd11d5b7c894a
Sevda <58671217+sevdasimsek@users.noreply.github.com>
2024-03-29 12:03:50 +0300

    Create CustomerRepository.java

---
+++ src/main/java/com/etiya/rentacar/dataAccess/abstracts/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.etiya.rentacar.dataAccess.abstracts;
+
+import com.etiya.rentacar.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
